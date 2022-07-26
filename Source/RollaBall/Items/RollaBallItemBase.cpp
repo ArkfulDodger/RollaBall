81916e10bf80733743158f4a64c9565d2b14fe86
Noah Reece <65669992+ArkfulDodger@users.noreply.github.com>
2022-07-26 19:17:19 -0400

    Initial Commit
    Added descriptive comments to RollaBallItemBase to test GitHub workflow

--- Source/RollaBall/Items/RollaBallItemBase.cpp
+++ Source/RollaBall/Items/RollaBallItemBase.cpp
@@ -11,8 +11,10 @@ ARollaBallItemBase::ARollaBallItemBase()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 
+	// Create Mesh Object in world
 	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
 
+	// Set the Mesh as the root
 	RootComponent = Mesh;
 }
 
@@ -21,12 +23,14 @@ void ARollaBallItemBase::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	// Call the OverlapBegin method when the Ball Item overlaps with something
 	Mesh->OnComponentBeginOverlap.AddDynamic(this, &ARollaBallItemBase::OverlapBegin);
 }
 
 void ARollaBallItemBase::OverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	// Collect the item if overlapping with a player
 	if (Cast<ARollaBallPlayer>(OtherActor) != nullptr)
 	{
 		Collected();
@@ -35,8 +39,8 @@ void ARollaBallItemBase::OverlapBegin(UPrimitiveComponent* OverlappedComponent, 
 
 void ARollaBallItemBase::Collected_Implementation()
 {
+	// retrieve game mode, and call item collection if successfully found
 	ARollaBallGameModeBase* GameMode = Cast<ARollaBallGameModeBase>(GetWorld()->GetAuthGameMode());
-
 	if (GameMode)
 	{
 		GameMode->ItemCollected();
